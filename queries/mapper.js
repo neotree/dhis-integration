6e1b805e300e764b745d6e9e80269ae110ed23b9
mbaradza <mbaradza1@gmail.com>
2024-04-16 22:03:09 +0200

    Enhance Mapping

--- queries/mapper.js
+++ queries/mapper.js
@@ -1,31 +1,101 @@
-module.exports= {
-    NEWBORN_COMPLICATIONS_ASPHYXIA: {'element':'vcEWpcFjEuU','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_NONE: {'element':'SytVoNgDYlf','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_OTHER:{'element':'Z9Q4qIjoePS','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_PREMATURITY: {'element':'UXTZkim1Npv','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_SEPSIS: {'element':'bPvw8ZdBgRe','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_LBW:  {'element':'gkgM1TgE5oF','categoryOptionCombo':""},
+module.exports = {
 
-    NEWBORN_COMPLICATIONS_MNGT_ANTIBIOTICS: {'element':'mWeTHZCPNT0','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_MNGT_KMC: {'element':'zANGFtNoUEt','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_MNGT_OTHER: {'element':'qYKuaHC8Gnk','categoryOptionCombo':""},
-    NEWBORN_COMPLICATIONS_MNGT_RESC: {'element':'zE0WLBSHx1e','categoryOptionCombo':""},
+    //NEW BORN COMPLICATIONS
+    RHD_MAT_NEWBORN_COMPLICATIONS_ASPHYXIA: { element: "vcEWpcFjEuU", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_NONE: { element: "SytVoNgDYlf", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_OTHER: { element: "Z9Q4qIjoePS", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_PREMATURITY: { element: "UXTZkim1Npv", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_SEPSIS: { element: "bPvw8ZdBgRe", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_LBW: { element: "gkgM1TgE5oF", categoryOptionCombo: "Tt7fU5lUhAU" },
 
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_EXP_NO_NVP: {'element':'EeywK6AHQdK','categoryOptionCombo':""},
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_EXP_NVP_STARTED: {'element':'iBBnHx1Uf50','categoryOptionCombo':""},
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_NEONATAL_DEATH: {'element':'jz793Wzon1W','categoryOptionCombo':""},
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_NOT_HIV_EXP: {'element':'q7NoDcfaciO','categoryOptionCombo':""},
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_STILL_FRESH: {'element':'YlydKJIyGFF','categoryOptionCombo':""},
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_UNKOWN_EXP: {'element':'kaYz6eqxqQH','categoryOptionCombo':""}, 
-    NEWBORN_SURVIVAL_PMTCT_ALIVE_STILLBIRTH_MASCERATED: {'element':'EJ0qYm1FrcH','categoryOptionCombo':""},
+    //NEW BOARN COMPLICATIONS MANAGEMENT
+    RHD_MAT_NEWBORN_COMPLICATIONS_MNGT_ANTIBIOTICS: { element: "mWeTHZCPNT0", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_MNGT_KMC: { element: "zANGFtNoUEt", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_MNGT_OTHER: { element: "qYKuaHC8Gnk", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_COMPLICATIONS_MNGT_RESC: { element: "zE0WLBSHx1e", categoryOptionCombo: "Tt7fU5lUhAU" },
 
-    ROUTINE_CARE_CHLOROHEXIDINE_GIVEN:{'element':'YvNy8rie5j7','categoryOptionCombo':""},
-    ROUTINE_CARE_ITN_GIVEN: {'element':'NNFGzJD43R9','categoryOptionCombo':""},
-    ROUTINE_CARE_VITK_GIVEN: {'element':'zAcW94iEdXU','categoryOptionCombo':""}, 
-    ROUTINE_CARE_TETRACYCLINE_GIVEN: '',
+    //NEW BORN SURVIVAL
 
-    BREAST_FEEDING_INITIATED: {'element':'bjLZY1FqdDJ','categoryOptionCombo':""}, 
-    BREAST_FEEDING_INITIATED_NEVER_OR_LATER_THAN_60: {'element':'bjLZY1FqdDJ','categoryOptionCombo':""},
-    BREAST_FEEDING_INITIATED_BFORE_60_MINS: ''
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_EXP_NO_NVP: { element: "EeywK6AHQdK", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_EXP_NVP_STARTED: { element: "iBBnHx1Uf50", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_NEONATAL_DEATH: { element: "jz793Wzon1W", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_NOT_HIV_EXP: { element: "q7NoDcfaciO", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_STILL_FRESH: { element: "YlydKJIyGFF", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_UNKOWN_EXP: { element: "kaYz6eqxqQH", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_NEWBORN_SURVIVAL_PMTCT_ALIVE_STILLBIRTH_MASCERATED: { element: "EJ0qYm1FrcH", categoryOptionCombo: "Tt7fU5lUhAU" },
+
+    //ROUTINE CARE
+    RHD_MAT_ROUTINE_CARE_CHLOROHEXIDINE_GIVEN: { element: "YvNy8rie5j7", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_ROUTINE_CARE_ITN_GIVEN: { element: "NNFGzJD43R9", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_ROUTINE_CARE_VITK_GIVEN: { element: "zAcW94iEdXU", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //STAFF
+    RHD_MAT_STAFF_CONDUCTING_DELIVERY: { element: "R9CHUW81m9o", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_STAFF_CONDUCTING_DELIVERY_OTHER: { element: "tNiIEkS6V9C", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_STAFF_CONDUCTING_DELIVERY_PA_WA_HSA: { element: "mHcrkMgxARq", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //TEO
+    RHD_MAT_CARE_TETRACYCLINE_GIVEN: { element: "YGJbvAkqBgI", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_CARE_TETRACYCLINE_NOT_GIVEN: { element: "FWgRucyjQRx", categoryOptionCombo: "Tt7fU5lUhAU" },
+
+    //BREAST FEEDING
+    BREAST_FEEDING_INITIATED_NEVER_OR_LATER_THAN_60: { element: "bjLZY1FqdDJ", categoryOptionCombo: "Tt7fU5lUhAU" },
+    BREAST_FEEDING_NEVER_INITIATED_OR_LESS_THAN_60_MINS: { element: "QiIfrKC5Ox8", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //DELIVERY
+    RHD_MAT_DELIVERY_IN_TRANSIT: { element: "KOu770F7YOQ", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_MODE_BREECH: { element: "UdxIE19B603", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_MODE_CEASARIAN_SECTION: { element: "FYDa4NcJNrq", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_MODE_SPONTANIOUS_VAGINAL: { element: "GwzEUK12Pqh", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_MODE_VACUUM_EXTRACTION: { element: "LLrXje6xH8D", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_PLACE_HOME_OR_TBA: { element: "hPhTbuLFnx4", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_PLACE_OTHER_FACILITY: { element: "iGYCZRe52Vy", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_DELIVERY_PLACE_THIS_FACILITY: { element: "YIP4fvYcGYQ", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //HIV
+    RHD_MAT_HIV_TEST_RESULTS_NEW_NAGATIVE: { element: "MD0Gp8MzkHf", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_HIV_TEST_RESULTS_NEW_POSITIVE: { element: "xRrJWogiEMf", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_HIV_TEST_RESULTS_NOT_DONE: { element: "U3FydV1M1RQ", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_HIV_TEST_RESULTS_PREVIOUS_NAGATIVE: { element: "PpmPCtwOWNR", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_HIV_TEST_RESULTS_PREVIOUS_POSITIVE: { element: "a3DzFDGAQAi", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //ART
+    RHD_MAT_ART_MOTHER_NOT_ON_ART: { element: "MoximPvXuj6", categoryOptionCombo: "GoX45uTUUCH", },
+    RHD_MAT_ART_MOTHER_STARTING_DURING_LABOUR: { element: "MoximPvXuj6", categoryOptionCombo: "hPVDsUM32VS", },
+    RHD_MAT_ART_MOTHER_START_IN_1ST_OR_2ND_TRIMESTER: { element: "MoximPvXuj6", categoryOptionCombo: "jMqmEmzruFg" },
+    RHD_MAT_ART_MOTHER_START_IN_3RD_TRIMESTER: { element: "MoximPvXuj6", categoryOptionCombo: "UyOtLy9DhPs" },
+    RHD_MAT_ART_MOTHER_START_BEFORE_PREGNANCY: { element: "MoximPvXuj6", categoryOptionCombo: "xmKtsiRYAXz" },
+    //OBSTETRIC COMPLICATIONS
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_ANTI_PARTUM_HEAM: { element: "RS3PtRniRns", categoryOptionCombo: "DfPxMVfjndd" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_PREMATURE_LABOUR: { element: "RS3PtRniRns", categoryOptionCombo: "ezMtOgig3e6" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_OBS_PRO_LABOUR: { element: "RS3PtRniRns", categoryOptionCombo: "Ht1Pz8Kn2k3" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_PRE_CLAMPSIA: { element: "RS3PtRniRns", categoryOptionCombo: "JPgETYeD4qF" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_SEPSIS: { element: "RS3PtRniRns", categoryOptionCombo: "N6Kp9pEEqSI" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_FEOTAL_DISTRESS: { element: "RS3PtRniRns", categoryOptionCombo: "PEpIz3uFJ4a" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_RETAINED_PLACENTA: { element: "RS3PtRniRns", categoryOptionCombo: "qffHiXEve5i" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_OTHERS: { element: "RS3PtRniRns", categoryOptionCombo: "R2qiSvs4zGK" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_RAPTURED_UTERUS: { element: "RS3PtRniRns", categoryOptionCombo: "VrBgkdWJor3" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_NONE: { element: "RS3PtRniRns", categoryOptionCombo: "XvMqbLpCTt8" },
+    RHD_MAT_OBSTETRIC_COMPLICATIONS_POST_PARTUM_HEAM: { element: "RS3PtRniRns", categoryOptionCombo: "ZGX8SK5P435" },
+    // EMERGENCE OBSTETRICS
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_ANTENATAL: { element: "b4WuKkuuR0W", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_ANTIBIOTICS: { element: "gXjGJoC7gDs", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_ANTIBIOTICS_ERP: { element: "p1OuCSVe4Gk", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_ANTIBIOTICS_ANTI_CONVULSIVE: { element: "n7XnfWv0tHo", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_BLOOD_TRANSFUSION: { element: "vyIPU3KOFF6", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_MRP: { element: "BbmxEK5AYaL", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_NASG: { element: "Tx8IDGqV3rt", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_EMERGENCE_OBSTETRIC_CARE_OXYTOCIN: { element: "FhNg0SdUMe8", categoryOptionCombo: "Tt7fU5lUhAU" },
+
+    //REFERAL 
+    RHD_MAT_REFERRED_OUT_NO: { element: "ViOAupIUYOG", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_REFERRED_OUT_YES: { element: "l2GWTPGK4QP", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //VITAMIN A
+    RHD_MAT_VITAMIN_A_ADMINISTRATION_GIVEN: { element: "B0MbTYjfUhd", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_VITAMIN_A_ADMINISTRATION_NOT_GIVEN: { element: "REbkEp8YboK", categoryOptionCombo: "Tt7fU5lUhAU" },
+    //MARTERNAL OUTCOME
+    RHD_MAT_MATERNAL_DEATHS: { element: "WjHvEHMCyKo", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_MOTHER_DIED_BEFORE_DISCHARGE: { element: "dTK0MlakKzU", categoryOptionCombo: "Tt7fU5lUhAU" },
+    RHD_MAT_MOTHER_ALIVE: { element: "qpSUfm5iTZo", categoryOptionCombo: "Tt7fU5lUhAU" },
+
+    //SINGLE, TWIN/ TRIPLETS
+    RHD_MAT_NUMBER_OF_BABIES_SINGLETONS: { element: "jZ5TP3UpaiQ", categoryOptionCombo: "PJEmG4BbHTf" },
+    RHD_MAT_NUMBER_OF_BABIES_TWINS_OR_TRIPLETS: { element: "jZ5TP3UpaiQ", categoryOptionCombo: "W3Angorx03z" },
+    RHD_TOTAL_NUMBER_OF_BABIES: { element: "NogxNBMHqK3", categoryOptionCombo: "Tt7fU5lUhAU" },
 
 }
