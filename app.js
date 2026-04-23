6a702e828d044c4a973ec6d867c511479ef40c21
mbaradza <mbaradza1@gmail.com>
2026-04-23 10:04:52 +0200

    Remove Unexpected Escape Characters

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const server = http.createServer(app);
 const dhisServices = require('./helper/cronJob');
 const port = Number(process.env.PORT) || 3009;
 
-
 app.get('/', (request, response) => {
   response.json({ info: 'WELLCOME TO NEOTREE-DHIS2 INTEGRATION' })
 })
@@ -21,6 +20,5 @@ dhisServices.updateSyncFailed();
 server.listen(port, () =>
   console.log(`Server is listening on port ${port}.`)
 )
-
 
 module.exports = app
